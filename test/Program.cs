050b2585353a640b453859c6fbf84b22c1a0215b
owtf086 <owtf086@gmail.com>
2024-11-03 17:48:25 +0300

    delete unused

--- test/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using IlyavtlnDotNet;
-
-var intro = new Introduction();
-Console.WriteLine("Creator Email: " + intro.CreatorEmail);
-Console.WriteLine("Creator GitHub: " + intro.CreatorGit);
